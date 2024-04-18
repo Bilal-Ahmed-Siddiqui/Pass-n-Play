00a1f348052c0c1f2c08afb51c9446235c8b0104
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-18 10:52:31 +0500

    auth token generation added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-//user creation POST '/api/auth/', doesnt req auth
+const JWT_SECRET_KEY = "thatsmykey";
+
+//sign up POST '/api/auth/', doesnt req auth
 router.post(
   "/",
   [
@@ -17,16 +20,24 @@ router.post(
     if (result.isEmpty()) {
       const { name, email, password } = req.body;
 
+      //hashing password
       const salt = await bcrypt.genSalt(10);
       const sec_pass = await bcrypt.hash(password, salt);
-      
+
+      // user creation
       const newUser = User({
         name: name,
         email: email,
         password: sec_pass,
       });
       newUser.save();
-      res.send("user created");
+
+      //token generation
+      const UID = {
+        id: newUser.id,
+      };
+      const authtoken = jwt.sign(UID, JWT_SECRET_KEY);
+      res.json({authtoken});
     } else {
       res.send("some internal error");
     }
