49688e7f13ed49756e5be7be815b7e3768884d0b
Bilal Ahmed <bilalkietacc@gmail.com>
2024-04-18 09:49:47 +0500

    user model connected to EP, Validation also applied

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+// const bcrypt = require('bcrypt');
 
-router.get("/", (req, res) => {
-  res.send("auth ep");
-});
+//user creation POST '/api/auth/', doesnt req auth
+router.post(
+  "/",
+  [
+    body("name").isLength({ min: 5 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      res.send("user created");
+      const { name, email, password } = req.body;
+      const newUser = User({
+        name: name,
+        email: email,
+        password: password,
+      });
+      newUser.save();
+    } else {
+      res.send("some internal error");
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
