a3f541d3f5da19cf8a2a7fe96d6549c0ff1aef5d
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-19 09:33:06 +0500

    fetchAll ep

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -6,4 +6,16 @@ router.post("/", (req, res) => {
   res.send("post ep");
 });
 
+//fetch all post, GET, '/api/post/fetchall', doesnt required auth
+
+router.get("/fetchall", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json({ posts: posts });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error!");
+  }
+});
+
 module.exports = router;
