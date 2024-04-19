e8da788c81b3a430b631739e6ddf524ba3d2ce47
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-19 12:13:22 +0500

    post updation end point created

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -70,4 +70,56 @@ router.get("/userposts", fetchUser, async (req, res) => {
   }
 });
 
+// Update user post, PUT, '/api/post/update/:id'
+router.put(
+  "/update/:id",
+  fetchUser,
+  [
+    body("title", "Title can not be empty").not().isEmpty(),
+    body("description", "Description can not be empty").not().isEmpty(),
+    body("rentPeriod", "Rent period can not be empty").not().isEmpty(),
+    body("location", "Location can not be empty").not().isEmpty(),
+    body("condition", "Condition can not be empty").not().isEmpty(),
+    body("price", "Price can not be empty").not().isEmpty(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors });
+      }
+      let post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).send("Post Not Found!");
+      }
+      if (req.user.id !== post.user.toString()) {
+        return res.status(401).send("UnAuthorized Access!");
+      }
+
+      const { title, description, rentPeriod, location, condition, price } =
+        req.body;
+      const updatedPost = {
+        user: req.user.id,
+        title,
+        description,
+        rentPeriod,
+        location,
+        condition,
+        price,
+      };
+
+      post = await Post.findByIdAndUpdate(
+        req.params.id,
+        { $set: updatedPost },
+        { new: true }
+      );
+
+      res.send("post updated");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error!");
+    }
+  }
+);
+
 module.exports = router;
