53e3e84b964e788943d87b2d53fe429207c5464e
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-20 14:50:11 +0500

    react-router-dom integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import Singup from "./components/Singup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      my app
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/Login" exact element={<Login />} />
+        <Route path="/Singup" exact element={<Singup />} />
+        <Route path="/About" exact element={<About />} />
+        <Route path="*" exact element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
