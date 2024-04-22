d924db184cd3a5aaf485dd4e5e8af880add74716
Bilal Ahmed <bilalkietacc@gmail.com>
2024-04-22 19:38:47 +0500

    notes context added, Post Ui, bug fixes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 import Signup from "./components/Signup";
+import PostDetails from "./components/PostDetails";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import postsContext, { PostsState } from "./context/postsContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/Login" exact element={<Login />} />
-        <Route path="/Signup" exact element={<Signup />} />
-        <Route path="/About" exact element={<About />} />
-        <Route path="*" exact element={<NotFound />} />
-      </Routes>
-    </Router>
+    <PostsState>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/Login" exact element={<Login />} />
+          <Route path="/Signup" exact element={<Signup />} />
+          <Route path="/About" exact element={<About />} />
+          <Route path="/PostDetails" exact element={<PostDetails />} />
+          <Route path="*" exact element={<NotFound />} />
+        </Routes>
+      </Router>
+    </PostsState>
   );
 }
 
