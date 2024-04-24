5ef42c3f47215d5be1c46a51ed12b8cf5c03a407
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-24 13:01:23 +0500

    some refactoring and user-ads fetch call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import Post from "./Post"
-const home = () => {
+import Post from "./Post";
+import postsContext from "../context/postsContext";
+
+const Home = () => {
+  const context = useContext(postsContext);
+  const { posts, fetchAll } = context;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetchAll();
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Failed to fetch data. Please try again later.");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <Navbar></Navbar>
-      <Post></Post>
+      <div className="post-container">
+        {loading ? (
+          <div className="preloader text-center container-fluid my-3">
+            <div
+              className="spinner-border"
+              style={{ width: "3rem", height: "3rem" }}
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        ) : error ? (
+          <div className="container">
+            <h5>{error}</h5>
+          </div>
+        ) : (
+          posts.map((post) => (
+            <Post
+              id={post._id}
+              title={post.title}
+              price={post.price}
+            ></Post>
+          ))
+        )}
+      </div>
     </>
   );
 };
 
-export default home;
+export default Home;
