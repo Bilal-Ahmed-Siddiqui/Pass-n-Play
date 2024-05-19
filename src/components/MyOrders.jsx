4741be595382ea79ddfca21172f87508eed67ace
bilal Ahmed <bilalkietacc@outlook.com>
2024-05-20 03:46:52 +0500

    Order Details completed

--- src/components/MyOrders.jsx
+++ src/components/MyOrders.jsx
@@ -86,7 +86,7 @@ const MyOrders = () => {
                 <div className="mt-4">
                   <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={() => navigate(`/order/${order._id}`)}
+                    onClick={() => navigate(`/orderdetails/${order._id}`)}
                   >
                     View Order
                   </button>
